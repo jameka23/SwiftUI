82a0dbadf92f11d1da0a7a0deb5c7432a13a4c85
Jameka Echols <jameka.echols@gmail.com>
2023-06-07 01:12:58 -0400

    created first couple of views

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,16 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .foregroundStyle(.green)
+                .bold()
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
         }
         .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct Previews_ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
